13ca8c71a87641c662f2ab8012ffdce95a704b86
jiyeonyooo <ji04230423@gmail.com>
2025-09-10 21:18:58 +0900

    variable mini quest1

---
+++ Java연습/variable-q1.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "green";
+        int age = 22;
+        double height = 161.154321;
+        String heightS = String.format("%.1f", height); //소수점 둘째자리에서 반올림
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(heightS);
+    }
+}
