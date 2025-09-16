6c5f2b77eb9217f6864f3ba85b7965c55660fc04
jiyeonyooo <ji04230423@gmail.com>
2025-09-16 16:06:08 +0900

    feat: make cardFactory class for applying facotry pattern

--- week2/src/main/java/factory/CardFactory.java
+++ week2/src/main/java/factory/CardFactory.java
@@ -1,2 +1,24 @@
-package factory;public class CardFactory {
+package factory;
+import card.*;
+
+public class CardFactory {
+
+    public Card getCard (int category, int cardNum) {
+        if (category == 1) {
+            return new MoneyCard(cardNum);
+        }
+        else if (category == 2) {
+            return new LoveCard(cardNum);
+        }
+        else if (category == 3) {
+            return new AcademicCard(cardNum);
+        }
+        else if (category == 4) {
+            return new TotalCard(cardNum);
+        }
+        else {
+            return null;
+        }
+    }
+
 }
