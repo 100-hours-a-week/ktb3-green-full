b05d54fc435e1ffc7ba9eb76764b7cb3f537494a
jiyeonyooo <ji04230423@gmail.com>
2025-09-19 00:33:39 +0900

    feat: define Timer class that implements Runnable interface

--- week2/src/main/java/timer/CountDownTimer.java
+++ week2/src/main/java/timer/CountDownTimer.java
@@ -1,2 +1,27 @@
-package timer;public class CountDownTimer {
+package timer;
+
+public class CountDownTimer implements Runnable {
+
+    private int sec;
+    private String purpose;
+    public CountDownTimer(int sec, String purpose) {
+        this.sec = sec;
+        this.purpose = purpose;
+    }
+    @Override
+    public void run() {
+        while(sec > 0) {
+            System.out.println(this.purpose+"까지 "+this.sec+"초 남았습니다...");
+            sec--;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (sec == 0) {
+                System.out.println("타이머 종료");
+            }
+        }
+    }
+
 }
