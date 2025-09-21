29970d05989364ef503b88af075642e3f6c4c534
jiyeonyooo <ji04230423@gmail.com>
2025-09-21 17:05:45 +0900

    refactor: convert Card interface to class and define result method

--- week2/src/main/java/card/Card.java
+++ week2/src/main/java/card/Card.java
@@ -1,6 +1,17 @@
 package card;
 
-public interface Card {
-    public void result();
+import java.util.Map;
 
+public class Card {
+
+    private final static String CARD_LINE = "✰--✰--✰--✰--✰--✰--✰--✰--✰--✰--✰--✰--✰--✰--✰--✰--✰--✰--✰--✰";
+    protected int cardNum;
+    protected Map<Integer, String> cards;
+
+    public void result() {
+        System.out.println("\n"+cardNum+"번 카드가 선택되었습니다!");
+        System.out.println("\n"+CARD_LINE);
+        System.out.println(cards.get(cardNum));
+        System.out.println(CARD_LINE+"\n");
+    }
 }
