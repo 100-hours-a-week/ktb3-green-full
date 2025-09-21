ea73a3150f9441e5eb7c91de5e324bb66954eeb0
jiyeonyooo <ji04230423@gmail.com>
2025-09-21 17:06:38 +0900

    refactor: set final static constant

--- week2/src/main/java/factory/CardFactory.java
+++ week2/src/main/java/factory/CardFactory.java
@@ -3,17 +3,23 @@ import card.*;
 
 public class CardFactory {
 
+    private final static int MONEY = 1;
+    private final static int LOVE = 2;
+    private final static int ACADEMIC = 3;
+    private final static int TOTAL = 4;
+
+
     public Card getCard (int category, int cardNum) {
-        if (category == 1) {
+        if (category == MONEY) {
             return new MoneyCard(cardNum);
         }
-        else if (category == 2) {
+        else if (category == LOVE) {
             return new LoveCard(cardNum);
         }
-        else if (category == 3) {
+        else if (category == ACADEMIC) {
             return new AcademicCard(cardNum);
         }
-        else if (category == 4) {
+        else if (category == TOTAL) {
             return new TotalCard(cardNum);
         }
         else {
