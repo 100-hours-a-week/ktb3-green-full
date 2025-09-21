b18c126a4027102ae3cf938d2bc6bd9d39bff4d1
jiyeonyooo <ji04230423@gmail.com>
2025-09-21 17:09:49 +0900

    feat: add countdown timer thread

--- week2/src/main/java/page/CardSelector.java
+++ week2/src/main/java/page/CardSelector.java
@@ -2,6 +2,7 @@ package page;
 
 import card.*;
 import factory.CardFactory;
+import timer.CountDownTimer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +11,9 @@ import java.util.Scanner;
 
 public class CardSelector extends Fortune {
 
-    int category;
-    int cardNum;
+    private final static int CARD_NUM = 8;
+    private final static int WAITING_SEC = 3;
+    private final int category;
     List<String> selectionOptions;
 
     public CardSelector(int category) {
@@ -22,17 +24,17 @@ public class CardSelector extends Fortune {
     }
     public void displayCards() {
         System.out.println("\n-------------------------------------------");
-        for (int j = 0; j < 8; j++) {
+        for (int j = 0; j < CARD_NUM; j++) {
             System.out.printf("%-6s", "|");
         }
         System.out.print("\n");
-        for (int j = 0; j < 7; j++) {
+        for (int j = 0; j < (CARD_NUM - 1); j++) {
             System.out.printf("%-6s", "|  "+(j+1));
 
         }
         System.out.printf("%-6s", "|  ");
         System.out.print("\n");
-        for (int j = 0; j < 8; j++) {
+        for (int j = 0; j < CARD_NUM; j++) {
             System.out.printf("%-6s", "|");
         }
         System.out.print("\n");
@@ -45,14 +47,15 @@ public class CardSelector extends Fortune {
             return random.nextInt(7)+1; //1~7 랜덤 숫자 발생
         }
         else { //수동 선택
-            System.out.println("원하는 카드 번호를 입력해주세요!");
             while (true) {
+                System.out.println("원하는 카드 번호를 입력해주세요!");
                 System.out.print("입력: ");
                 int selectNum = scan.nextInt();
-                System.out.println("\n＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊\n");
-                if (selectNum > 0 && selectNum < 8) return selectNum;
+                if (selectNum > 0 && selectNum < 8) {
+                    System.out.println(LINE);
+                    return selectNum;
+                }
             }
-
         }
 
     }
@@ -61,12 +64,22 @@ public class CardSelector extends Fortune {
         System.out.println("총 7장의 카드 중 원하시는 카드를 고를 차례예요.\n");
         System.out.println("고르기 어려우시다면 랜덤으로 선택할 수 있는 1번을,\n직접 선택하시고 싶으시다면 2번을 입력해주세요!");
         displayCards();
-        int isRandom = receiveUserInput(selectionOptions, scan);
-        System.out.println("\n＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊\n");
-        cardNum = selectCard(isRandom == 1, scan);
-        CardFactory cardFactory = new CardFactory();
 
+        int isRandom = receiveUserInput(selectionOptions, scan);
+        System.out.println(LINE);
+        int cardNum = selectCard(isRandom == 1, scan);
+
+        CardFactory cardFactory = new CardFactory();
         Card selectedCard = cardFactory.getCard(this.category, cardNum);
+
+        Thread countDownTimer = new Thread(new CountDownTimer(WAITING_SEC, "카드공개"));
+        countDownTimer.start();
+        try {
+            countDownTimer.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         selectedCard.result();
     }
 }
