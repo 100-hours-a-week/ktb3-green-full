d406826592f3079dd0620434ecc48984eb60a87e
jiyeonyooo <ji04230423@gmail.com>
2025-10-27 15:45:21 +0900

    chore: move spring-community under week4

--- spring-community/src/main/java/com/example/spring_community/Auth/dto/LoginDto.java
+++
@@ -1,5 +0,0 @@
-package com.example.spring_community.Auth.dto;
-
-
-public record LoginDto(String email, String password) {
-}
--- spring-community/src/main/java/com/example/spring_community/Auth/dto/TokenDto.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring_community.Auth.dto;
-
-import lombok.Builder;
-
-@Builder
-public record TokenDto (String accessToken, String refreshToken){
-}
